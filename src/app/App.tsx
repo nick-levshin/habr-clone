cdeb0ed227b1c03799fd56840ef8c0573f75b581
Nick Levshin <nikita.levshin2016@gmail.com>
2023-12-03 15:28:06 +0300

    feat: add feature sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Suspense } from "react";
 import { Route, Routes, Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { AboutPage } from "@/pages/AboutPage";
+import { MainPage } from "@/pages/MainPage";
+import { classNames } from "@/shared/lib/classNames/classNames";
+import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
 import "./styles/index.scss";
 
 const App = ({}) => {
@@ -18,8 +18,8 @@ const App = ({}) => {
       <Link to="/about">About Us</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
