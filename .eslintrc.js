7efabdc2edb7ba7fc476cfd405a084636fca2f37
Nick Levshin <nikita.levshin2016@gmail.com>
2024-05-26 13:47:56 +0300

    fix: eslint warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,12 +36,23 @@ module.exports = {
     'react/function-component-definition': 'off',
     'react/jsx-props-no-spreading': 'warn',
     'no-shadow': 'off',
-    'import/no-extraneous-dependencies': 'warn',
+    'import/no-extraneous-dependencies': ['warn', { devDependencies: true }],
     'no-underscore-dangle': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'i18next/no-literal-string': [
+      'error',
+      { markupOnly: true, ignoreAttribute: ['data-testid'] },
+    ],
     'max-len': ['error', { ignoreComments: true, code: 120 }],
   },
   globals: {
     __IS_DEV__: true,
   },
+  overrides: [
+    {
+      files: ['**/src/**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+      },
+    },
+  ],
 };
